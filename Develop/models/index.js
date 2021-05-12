e5c64379ae8970e5faa3a6364339d6699868783d
Jonathan Rein <jrein1296@gmail.com>
2021-05-12 13:00:25 -0400

    Updating model associations in models/index.js.  These associations may need further development.

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,13 +4,29 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 
-// Categories have many Products
+// Associations below may be incomplete.  Consider renaming foreign keys to more descriptive names.
+// Products belongsTo Category
+Product.hasOne(Category, {
+  foreignKey: 'id',
+})
+
+// Categories have many Products 
+Category.hasMany(Product, {
+  foreignKey: 'id',
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'id',
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'id',
+})
 
 module.exports = {
   Product,
